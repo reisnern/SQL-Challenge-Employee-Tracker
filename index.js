27a072e8bc98fb59a8b624a8f3fd62c209bfc3d6
Nick Reisner <nreisne@wgu.edu>
2021-06-04 16:12:37 -0500

    Adding code and fixing JS

--- index.js
+++ index.js
@@ -13,3 +13,53 @@ const db = mysql.createConnection(
     },
     console.log("connected to the Employee Tracker database.")
   );
+  db.connect(function (err) {
+    if (err) throw err;
+    promptUser();
+  });
+  
+  // start inquirer
+  
+  function promptUser() {
+    console.log(`
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      *******EMPLOYEE TRACKER*******
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      `);
+  
+    return (
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "startTracker",
+            message: "What would you like to do?",
+            choices: [
+              "view all departments",
+              "view all roles",
+              "view all employees",
+              "add a department",
+              "add a role",
+              "add an employee",
+              "update an employee role",
+            ],
+          },
+        ])
+  
+        // need to set up what to do with the selected answer
+  
+        .then((promptAnswer) => {
+          switch (promptAnswer.startTracker) {
+            case "view all departments":
+              allDep();
+              break;
+  
+            case "view all roles":
+              allRoles();
+              break;
+  
+            case "view all employees":
+              allEmp();
+              break;
