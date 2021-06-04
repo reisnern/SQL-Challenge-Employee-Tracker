9c9e67a2c3433e991224b0e0422eb3e633143cea
Nick Reisner <nreisne@wgu.edu>
2021-06-04 15:18:19 -0500

    Added js

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const mysql = require("mysql");
 const CTable = require("console.table");
 
 //const db = require('./db/connection')
+const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "Raptor10!",
+      database: "employee_tracker",
+    },
+    console.log("connected to the Employee Tracker database.")
+  );
